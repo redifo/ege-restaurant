c52d7496d68fd06b0f10847e9291f6ded74b7757
Redifo <kaanredif@gmail.com>
2024-03-24 13:21:40 +0100

    Move js code related to signup page to a seperate js file.

--- static/js/main.js
+++ static/js/main.js
@@ -4,10 +4,3 @@ function goBack() {
 
 $("#copyright").text(new Date().getFullYear());
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Update placeholders and labels
-    document.getElementsByName("username")[0].setAttribute("placeholder", "Name");
-    
-    document.querySelector("label[for='id_username']").textContent = "Name:";
-    
-});
