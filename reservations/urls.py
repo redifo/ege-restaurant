0a01e0ee903b4c1e54529c75e3cc6b5c64c06e36
Redifo <kaanredif@gmail.com>
2024-03-24 13:14:54 +0100

    Create a edit reservations page where users can edit the reservation they just made

--- reservations/urls.py
+++ reservations/urls.py
@@ -4,5 +4,5 @@ from .views import make_reservation, reservation_success, edit_reservation
 urlpatterns = [
     path('', make_reservation, name="reservations"),
     path('success/<int:pk>', reservation_success, name="reservation_success"),
-    path('edit/', edit_reservation, name="edit_reservation")
+    path('reservation/edit/<int:pk>/', edit_reservation, name="edit_reservation")
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -10,16 +10,24 @@ def make_reservation(request):
             if request.user.is_authenticated:
                 reservation.customer = request.user
             reservation.save()
-            # Redirect to a new URL:
-            return redirect('reservation_success')  
+            # Redirect to the reservation_success URL with the reservation's pk
+            return redirect('reservation_success', pk=reservation.pk)  # Include pk in redirect
     else:
         form = ReservationForm()
     return render(request, 'reservations.html', {'form': form})
 
 def reservation_success(request, pk):
     reservation = get_object_or_404(Reservation, pk=pk)
-    return render(request, 'reservations/reservation_success.html', {'reservation': reservation})
+    return render(request, 'reservation_success.html', {'reservation': reservation})
 
 
 def edit_reservation(request, pk):
-    pass
+    reservation = get_object_or_404(Reservation, pk=pk)
+    if request.method == "POST":
+        form = ReservationForm(request.POST, instance=reservation)
+        if form.is_valid():
+            form.save()
+            return redirect('reservation_success', pk=reservation.pk)
+    else:
+        form = ReservationForm(instance=reservation)
+    return render(request, 'edit_reservation.html', {'form': form, 'reservation': reservation})
