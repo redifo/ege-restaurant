4534047a1596aeb108c8770a3c3569f8df64825a
Redifo <kaanredif@gmail.com>
2024-03-25 11:54:11 +0100

    fix the issue with reservation views.py not showing past and future reservations on page

--- reservations/views.py
+++ reservations/views.py
@@ -1,21 +1,38 @@
+from django import forms
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Reservation
 from .forms import ReservationForm
 from django.utils import timezone
 
 def make_reservation(request):
+    # Initialize past and future reservations only if user is authenticated
+    past_reservations = future_reservations = None
+    if request.user.is_authenticated:
+        past_reservations = Reservation.objects.filter(email=request.user.email, date__lt=timezone.now()).order_by('date', 'time')
+        future_reservations = Reservation.objects.filter(email=request.user.email, date__gte=timezone.now()).order_by('date', 'time')
     if request.method == 'POST':
         form = ReservationForm(request.POST)
         if form.is_valid():
             reservation = form.save(commit=False)
             if request.user.is_authenticated:
                 reservation.customer = request.user
+                reservation.email = request.user.email
             reservation.save()
             # Redirect to the reservation_success URL with the reservation's pk
             return redirect('reservation_success', pk=reservation.pk)  # Include pk in redirect
     else:
-        form = ReservationForm()
-    return render(request, 'reservations.html', {'form': form})
+        # Initialize the form with the email pre-filled for authenticated users
+        initial_data = {'email': request.user.email} if request.user.is_authenticated else {}
+        form = ReservationForm(initial=initial_data)
+        if request.user.is_authenticated:
+            # Hide the email field
+            form.fields['email'].widget = forms.HiddenInput()
+
+    return render(request, 'reservations.html', {
+            'form': form,
+            'past_reservations': past_reservations,
+            'future_reservations': future_reservations
+        })
 
 def reservation_success(request, pk):
     reservation = get_object_or_404(Reservation, pk=pk)
@@ -33,11 +50,3 @@ def edit_reservation(request, pk):
         form = ReservationForm(instance=reservation)
     return render(request, 'edit_reservation.html', {'form': form, 'reservation': reservation})
 
-def user_reservations(request):
-    if request.user.is_authenticated:
-        # Fetch reservations that are in the future
-        reservations = Reservation.objects.filter(customer=request.user, date__gte=timezone.now()).order_by('date', 'time')
-        return render(request, 'user_reservations.html', {'reservations': reservations})
-    else:
-        # Redirect to login or another appropriate page
-        pass
