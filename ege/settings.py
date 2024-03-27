d5ee9cc51118907ed16a3a7229420a3c99c2c2c1
Redifo <kaanredif@gmail.com>
2024-03-27 13:15:31 +0100

    Set Debug to false for deploy test

--- ege/settings.py
+++ ege/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com',
                  '127.0.0.1',]
