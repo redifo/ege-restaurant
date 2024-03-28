051e320aacf4768bfeb69385b8567ef6851c8317
Redifo <kaanredif@gmail.com>
2024-03-28 14:22:30 +0100

    run command python manage.py collectstatic --clear to fix missing favicon causing error in heroku

--- staticfiles/js/main.c8193216b65d.js
+++
@@ -1,9 +0,0 @@
-function switchForm(form) {
-    if(form === 'login') {
-        $('#login-form').show();
-        $('#register-form').hide();
-    } else {
-        $('#login-form').hide();
-        $('#register-form').show();
-    }
-}
